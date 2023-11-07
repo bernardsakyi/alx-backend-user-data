22b05ff770ba26359fce5ea8aa5afe5d7a5dd0ee
bernardsakyi <sakyibernard77@gmail.com>
2023-11-07 12:44:58 +0000

    1 file makes the difference

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,120 +1,87 @@
 #!/usr/bin/env python3
-"""
-Filter personal information using a Regex, Environmental variables, etc.
-"""
-
+""" Write a function called filter_datum that returns log """
+import logging
 import re
 from typing import List
-import logging
+from os import getenv
 import mysql.connector
-import os
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:  # nopep8
+    """ Filter logging """
+    for i in fields:
+        message = re.sub(fr'{i}=.+?{separator}', f'{i}={redaction}{separator}', message)  # nopep8
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """
-    Redacting Formatter class
-    """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        self.fields = fields
+        """ Initialize the formatter """
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields: List[str] = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """
-        Filter values in incoming log records using filter_datum.
-        Values for fields in fields should be filtered.
-        """
-        result = logging.Formatter(self.FORMAT).format(record)
+        """ format logrecord """
         return filter_datum(
             self.fields,
             self.REDACTION,
-            result,
+            super(RedactingFormatter, self).format(
+                record),
             self.SEPARATOR)
 
 
-PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    Connect to mysql server with environmental vars
-    """
-    db_connect = mysql.connector.connect(
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
-        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME')
-    )
-    return db_connect
-
-
-def filter_datum(fields: List[str],
-                 redaction: str,
-                 message: str,
-                 separator: str) -> str:
-    """
-    Returns the log message obfuscated with Regex
-    """
-    for item in fields:
-        message = re.sub(item + '=.*?' + separator, item + '=' +
-                         redaction + separator, message)
-    return message
-
-
 def get_logger() -> logging.Logger:
-    """
-    Returns a Logger object with a
-    StreamHandler with RedactingFormatter
-    as formatter
-    """
-    logger = logging.getLogger('user_data')
-    logger.setLevel(logging.INFO)
+    """ get logger for csv file """
+    logger = logging.getLogger("user_data")
+    stream = logging.StreamHandler()
+    stream.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream)
     logger.propagate = False
-
-    target_handler = logging.StreamHandler()
-    target_handler.setLevel(logging.INFO)
-
-    formatter = RedactingFormatter(list(PII_FIELDS))
-    target_handler.setFormatter(formatter)
-
-    logger.addHandler(target_handler)
+    logger.setLevel(logging.INFO)
     return logger
 
 
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ returns a connector to the database """
+    cnx = mysql.connector.connect(
+        user=getenv('PERSONAL_DATA_DB_USERNAME'),
+        password=getenv('PERSONAL_DATA_DB_PASSWORD'),
+        host=getenv('PERSONAL_DATA_DB_HOST'),
+        database=getenv('PERSONAL_DATA_DB_NAME'))
+    return cnx
+
+
 def main() -> None:
-    """
-    Obtain a database connection using get_db
-    and retrieve all rows in the users table and display each row w/ format:
+    """ generate mysql connector to the database """
+    drop = get_db()
+    cursor = drop.cursor()
+    query = "SELECT * FROM users"
+    cursor.execute(query)
+    formatter = get_logger()
+    for (
+        name,
+        email,
+        ssn,
+        phone,
+        password,
+        ip,
+        last_login,
+            user_agent) in cursor:
+        message = f"name={name}; email={email}; phone={phone}; ssn={ssn}; password={password}; ip={ip}; last_login={last_login}; user_agent={user_agent};"  # nopep8
+        formatter.info(message)
 
-    [HOLBERTON] user_data INFO 2019-11-19 18:37:59,596:
-    name=***; email=***; phone=***; ssn=***; password=***;
-    ip=60ed:c396:2ff:244:bbd0:9208:26f2:93ea; last_login=2019-11-14 06:14:24
-    user_agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)
-    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36;
-    """
-    db = get_db()
-    cur = db.cursor()
-
-    query = ('SELECT * FROM users;')
-    cur.execute(query)
-    fetch_data = cur.fetchall()
-
-    logger = get_logger()
-
-    for row in fetch_data:
-        fields = 'name={}; email={}; phone={}; ssn={}; password={}; ip={}; '\
-            'last_login={}; user_agent={};'
-        fields = fields.format(row[0], row[1], row[2], row[3],
-                               row[4], row[5], row[6], row[7])
-        logger.info(fields)
-
-    cur.close()
-    db.close()
+    cursor.close()
+    drop.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
